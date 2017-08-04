a9b61f164fd518987e60cdfacc5d8daed14514cd
Andrés Felipe Ceballos M <30534988+andresceballosm@users.noreply.github.com>
2017-08-04 12:05:04 -0500

    Update services.js

--- www/js/services.js
+++ www/js/services.js
@@ -5,8 +5,8 @@ angular.module('starter.services',[])
         WC: function(){
             var Woocommerce = new WooCommerceAPI.WooCommerceAPI({
                 url: 'http://www.efruver.com',
-                consumerKey: 'ck_9ccdde95b1e6842d0a422b95bf449f66556e2c25',
-                consumerSecret: 'cs_90eddbf55b98d7203c8353e5c12bfca4153ce71c'
+                consumerKey: 'xxxxxxxxxxxxxxxxxxxxx',
+                consumerSecret: 'xxxxxxxxxxxxxxxx'
             });
             return Woocommerce;
         }
